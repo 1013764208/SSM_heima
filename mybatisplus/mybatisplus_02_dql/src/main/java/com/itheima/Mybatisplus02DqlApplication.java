74e0308019bc90723a25b0e40636644927e867cd
1013764208 <1013764208@qq.com>
2023-10-05 10:34:05 +0800

    1005

--- mybatisplus/mybatisplus_02_dql/src/main/java/com/itheima/Mybatisplus02DqlApplication.java
+++ mybatisplus/mybatisplus_02_dql/src/main/java/com/itheima/Mybatisplus02DqlApplication.java
@@ -1,13 +1,13 @@
-package com.heima;
+package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot01QuickstartApplication {
+public class Mybatisplus02DqlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot01QuickstartApplication.class, args);
+        SpringApplication.run(Mybatisplus02DqlApplication.class, args);
     }
 
 }
